1acb62e10b5ccaa4c8cc1cfabc9ffcaa636b3c03
George <george@MacBook-Air-von-George.local>
2024-12-30 00:06:22 +0100

    Move consultations.js to api directory for serverless deployment

--- start-server.js
+++ start-server.js
@@ -5,7 +5,7 @@ const port = 4000;
 
 // Function to check if the port is in use and kill the process
 function checkAndKillPort(port) {
-    const command = process.platform === 'win32' ? `netstat -ano | findstr :${port}` : `lsof -i :${port}`;
+    const command = process.platform === 'win32' ? `netstat -ano | findstr :${port}` : `lsof -ti :${port}`;
     exec(command, (err, stdout, stderr) => {
         if (err) {
             console.error(`Error checking port: ${stderr}`);
@@ -13,8 +13,7 @@ function checkAndKillPort(port) {
         }
         if (stdout) {
             console.log(`Command output: ${stdout}`);
-            const lines = stdout.trim().split('\n');
-            const pid = process.platform === 'win32' ? lines[lines.length - 1].trim().split(/\s+/)[4] : lines[0].trim().split(/\s+/)[1];
+            const pid = stdout.trim();
             console.log(`Extracted PID: ${pid}`);
             if (pid && !isNaN(pid)) {
                 const killCommand = process.platform === 'win32' ? `taskkill /PID ${pid} /F` : `kill -9 ${pid}`;
@@ -46,5 +45,6 @@ function startServer() {
         console.log(stdout);
     });
 }
+
 // Run the check and kill process
 checkAndKillPort(port);
