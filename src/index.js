b63827cc8ca7bef4851531556a98cc81215d3fbf
G <41178744+g-but@users.noreply.github.com>
2025-01-13 23:34:25 +0100

    Add `src/index.js` to render `ConsultationForm` component
    * Import React and ReactDOM
    * Import `ConsultationForm` component
    * Render `ConsultationForm` component into `consultationFormContainer` div

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ConsultationForm from './components/ConsultationForm';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <ConsultationForm />
+  </React.StrictMode>,
+  document.getElementById('consultationFormContainer')
+);
