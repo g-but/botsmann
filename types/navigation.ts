6ca4221cc1814d86f1e95751df40d5e72b38eb1f
G <41178744+g-but@users.noreply.github.com>
2025-06-13 16:23:39 +0200

    refactor navigation with mega menu

--- types/navigation.ts
+++ types/navigation.ts
@@ -3,6 +3,10 @@ import { LinkProps } from 'next/link';
 export interface MenuItem {
   label: string;
   path: LinkProps<string>['href'];
+  /**
+   * Optional description for mega menu display
+   */
+  description?: string;
   children?: MenuItem[];
   isButton?: boolean;
 }
